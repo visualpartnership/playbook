f56c2d4ffc75c0ac00705a876c46030bc83a8717
Jibhran Aguilar <71751568+JibhranLimon96@users.noreply.github.com>
2022-04-10 16:39:17 -0500

    Create logger.js

---
+++ weekly_mission_1/Example-9/logger.js
@@ -0,0 +1,17 @@
+/*
+  export default nos permite exportar esta clase e importara
+  import MyLogger from './logger.js'
+*/
+
+export default class Logger {
+  constructor (name) {
+    this.name = name
+  }
+
+  log (message) {
+    console.log(`[${this.name}] ${message}`)
+  }
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
