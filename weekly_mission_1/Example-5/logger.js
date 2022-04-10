963129fdefc54b5366733f60b14c296b78c80240
Jibhran Aguilar <71751568+JibhranLimon96@users.noreply.github.com>
2022-04-10 16:23:22 -0500

    Create logger.js

---
+++ weekly_mission_1/Example-5/logger.js
@@ -0,0 +1,14 @@
+class Logger {
+  constructor(name){
+    // Al crear este objeto se guardarán estos valores
+    this.count = 0
+    this.name = name
+  }
+
+  log(message) {
+    this.count++ // se aumenta el contador interno al llamar este método
+    console.log('[' + this.name + '] ' + message)
+  }
+}
+
+module.exports = new Logger('DEFAULT') // Instanciación del objeto y se exporta
