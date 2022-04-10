e59b4db8a735801b8ab31528c4c926c73e35e89c
Jibhran Aguilar <71751568+JibhranLimon96@users.noreply.github.com>
2022-04-10 16:37:55 -0500

    Create pokemon-3.js

---
+++ weekly_mission_1/Example-9/pokemon-3.js
@@ -0,0 +1,19 @@
+const Pokemon = require('./pokemon')
+
+const pikachu = new Pokemon("pikachu")
+const bulbasaur = new Pokemon("bulbasaur")
+const squirtle = new Pokemon("squirtle")
+const charmander = new Pokemon("charmander")
+
+pikachu.sayHello()
+pikachu.sayMessage("Heey!")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage("Heey!")
+
+charmander.sayHello()
+charmander.sayMessage("Heey!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heey!")
+
