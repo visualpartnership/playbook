1685b4ca87de1e5744be96a79a8b26ddf9c38c65
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-12 00:07:56 -0500

    Agregando ejercicio 6

---
+++ weekly_mission_1/example6/main.js
@@ -0,0 +1,6 @@
+//Example 6: Modificación de clases
+
+require('./patcher') // Llamas este módulo que modifica el objeto instanciado
+const logger = require('./logger') // Al llamar el módulo en logger.js te dará el objeto modificado
+
+logger.customMessage()
