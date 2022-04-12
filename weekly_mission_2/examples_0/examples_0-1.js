4dae7a21f1d1bb945123306a84c0b575ecb19004
Kevin_Salgado <sgkevinsg@gmail.com>
2022-04-12 13:43:45 -0500

    examples0

---
+++ weekly_mission_2/examples_0/examples_0-1.js
@@ -0,0 +1,4 @@
+// Ejemplo 1: Crear un objeto
+const myObjetc = {} // Esto es un objeto vacío
+console.log("Ejemplo 1: Crear un objeto vacío")
+console.log(myObjetc)
---
+++ weekly_mission_2/examples_0/examples_0-3.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Tulio",
+    age: 2,
+    nicknames: [
+     "Tulipan",
+      "Tulancingo",
+      "Tulish"
+    ],
+    address: {
+      zip_code: "10000",
+      street: "Dr. Vertiz 11 Benito Juarez",
+      city: "CDMX"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)
+  console.log(myObject3.name)
+  console.log(myObject3["address"])
