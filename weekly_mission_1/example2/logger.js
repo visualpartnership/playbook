5fee02da030e4453fb3e986947ac929c7a5447ef
MikoVelRa <devdayarturo@gmail.com>
2022-04-12 13:09:01 -0500

    feat: excercise 2

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,7 @@
+// logger.js
+
+// Esta es una función que se guardara en este módulo como info
+
+exports.info = message => console.log(`Info: ${message}`)
+
+exports.verbose = message => console.log(`Verbose: ${message}`)
