9875d7f8b8695fd855b03c21127250d401eae2f4
MikoVelRa <devdayarturo@gmail.com>
2022-04-12 16:55:04 -0500

    feat: excercise 9

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,15 @@
+class Pokemon{
+    constructor(name){
+        this.name = name;
+    }
+
+    sayHello(){
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+    }
+
+    sayMessage(message){
+        console.log(`Mi pokemon ${this.name} dice: ${message}`)
+    }
+}
+
+module.exports = Pokemon;
