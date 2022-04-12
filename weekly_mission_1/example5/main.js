c3112f63dbc8a58c53bf4f06bf293ac059f94158
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-12 00:07:14 -0500

    Agregando ejercicio 5

---
+++ weekly_mission_1/example5/main.js
@@ -0,0 +1,15 @@
+//Example 5: Clases y Objetos
+
+const logger = require('./logger')
+
+// Ya se puede usar directamente el objeto instanciado en el módulo logger
+logger.log('This is an informational message')
+
+/*
+También pueder instanciar uno nuevo de esta manera:
+  const customLogger = new logger.constructor('CUSTOM')
+  customLogger.log('This is an informational message')
+*/
+
+const customLogger = new logger.constructor('CUSTOM')
+customLogger.log('This is an informational message')
