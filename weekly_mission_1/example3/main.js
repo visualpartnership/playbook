264a154ba1ee85fcd5887ac35257c8c2b274b694
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-12 00:02:56 -0500

    Agregando ejercicio 3

---
+++ weekly_mission_1/example3/main.js
@@ -0,0 +1,11 @@
+//Example 3: Diferentes formas de exportar funciones
+
+/*
+  node main.js
+*/
+
+const logger1 = require('./logger_1')
+const logger2 = require('./logger_2')
+
+logger1('This is an informational message')
+logger2.verbose('This is a verbose message')
