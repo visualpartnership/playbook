20310b93c30829f766a4abc8091db5b222d8f65b
Gustavo-Valdez <86739150+Gustavo-Valdez@users.noreply.github.com>
2022-04-13 23:40:29 -0500

    mm

--- weekly_mission_1/example2/logger.js
+++ weekly_mission_1/example2/logger.js
@@ -2,17 +2,17 @@
 
 // Esta es una función que se guardara en este módulo como info
 exports.info = (message) => {
-    console.log(`info: ${message}`)
-  }
-  
-  // Esta es una función que se guardara en este módulo como verbose
-  exports.verbose = (message) => {
-    console.log(`verbose: ${message}`)
-  }
-  
-  /*
-    const logger = require('./logger')
-    logger.info('This is an informational message')
-    logger.verbose('This is a verbose message')
-   * */
+  console.log(`info: ${message}`)
+}
+
+// Esta es una función que se guardara en este módulo como verbose
+exports.verbose = (message) => {
+  console.log(`verbose: ${message}`)
+}
+
+/*
+  const logger = require('./logger')
+  logger.info('This is an informational message')
+  logger.verbose('This is a verbose message')
+ * */
   
