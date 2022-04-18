96ed15f34b3e7a22ecb974a4283b60f3d91115e2
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-18 17:32:42 -0500

    Agregando ejemplo 1 de examples1.js

---
+++ weekly_mission_2/examples1/examples1-1.js
@@ -0,0 +1,11 @@
+console.log("Operadores")
+
+console.log("--------------------------------------------------------")
+
+console.log("Ejemplo 1: for Each para imprimir elementos de una lista")
+
+console.log("--------------------------------------------------------")
+
+const numbers = [1, 2, 3, 4, 5]
+console.log("Ejemplo 1: Imprimiendo los elementos de la lista")
+numbers.forEach(num => console.log(num))
