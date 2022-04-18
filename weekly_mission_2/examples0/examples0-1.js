06d82484d6cd868c01fbb2a1bc30a38d1abb6a77
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-17 23:40:59 -0500

    Agregando ejempl 1 de examples0.js

---
+++ weekly_mission_2/examples0/examples0-1.js
@@ -0,0 +1,11 @@
+console.log("Objetos")
+
+console.log("--------------------------------------------")
+
+console.log("Ejemplo 1: Crear un objeto")
+
+console.log("--------------------------------------------")
+
+const myObject = {} // Esto es un objeto vacío
+console.log("Ejemplo 1: Esto es un objeto vacío")
+console.log(myObject)
