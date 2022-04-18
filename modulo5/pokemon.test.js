60759e015d17d4a99490abfefe85f34a414ac9b2
Yael Villarreal <iaeloko@gmail.com>
2022-04-18 01:09:40 -0500

    termino de ejercicios de ejemplo

---
+++ modulo5/pokemon.test.js
@@ -0,0 +1,6 @@
+import Pokemon from './pokemon'
+
+test('1) Create a new object pokemon', () => {
+  const myPokemon = new Pokemon('Pikachu')
+  expect(myPokemon.name).toBe('Pikachu'); // Corrige esta prueba
+});
