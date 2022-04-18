7e60590063e44581ad18353d3d0ebc05baf4f9e3
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-18 17:42:10 -0500

    Agregando ejemplo 2 de examples1.js

---
+++ weekly_mission_2/examples1/examples1-2.js
@@ -0,0 +1,13 @@
+console.log("Operadores")
+
+console.log("--------------------------------------------------------")
+
+console.log("Ejemplo 2: for Each para calcular la suma de todos los elementos de una lista")
+
+console.log("--------------------------------------------------------")
+
+let sum = 0
+const numbers2 = [1, 2, 3, 4, 5]
+numbers2.forEach(num => sum += num) // numbers2.forEach(num => sum = sum+num)
+console.log("Ejemplo 2: Cálculo de la suma de los elementos de la lista")
+console.log(sum)
