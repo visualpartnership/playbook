edccb833cf6653a8e66aeae2eeff4e44d1e0af6a
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-17 23:49:02 -0500

    Agregando ejemplo 2 de examples0.js

---
+++ weekly_mission_2/examples0/examples0-2.js
@@ -0,0 +1,14 @@
+console.log("Objetos")
+
+console.log("--------------------------------------------")
+
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+
+console.log("--------------------------------------------")
+
+const myObject2 = {
+    name: "Gloria",
+    age: 23
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObject2)
