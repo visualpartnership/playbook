b2c2a74e3edceccccd64be061d9331cfc20fd04d
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-19 16:23:48 -0500

    Agregando ejemplo 1 de examples2.js

---
+++ weekly_mission_2/examples2/examples2-1.js
@@ -0,0 +1,13 @@
+console.log("Clases y objetos")
+
+console.log("----------------------------------------------")
+
+console.log("Ejemplo 1: Crear una clase vacía")
+
+console.log("----------------------------------------------")
+
+class Person{
+
+}
+console.log("Ejemplo 1: Crear una clase vacía")
+console.log(Person) // [class Person]
