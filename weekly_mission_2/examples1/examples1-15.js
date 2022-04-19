4baba59ea18fb2c89cd885df6f8898d2a468acec
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-19 02:40:46 -0500

    Agregando ejemplo 15 de examples1.js

---
+++ weekly_mission_2/examples1/examples1-15.js
@@ -0,0 +1,11 @@
+console.log("Operadores")
+
+console.log("--------------------------------------------------------")
+
+console.log("Ejemplo 15: Uso de sort para ordenar elementos")
+
+console.log("--------------------------------------------------------")
+
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot']
+console.log("Ejemplo 15: Elementos ordernados usando SORT")
+console.log(products.sort())
