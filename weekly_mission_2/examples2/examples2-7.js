89f19fa3b60844c95fb311b4a18a62956d609ab5
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-19 18:51:10 -0500

    Agregando ejemplo 7 de examples2.js

---
+++ weekly_mission_2/examples2/examples2-7.js
@@ -0,0 +1,38 @@
+console.log("Clases y objetos")
+
+console.log("----------------------------------------------")
+
+console.log("Ejemplo 7: Setters para modificar los atributos del objeto")
+
+console.log("----------------------------------------------")
+
+class MissionCommander{
+    constructor(name, mission){
+        this.name = name
+        this.mission = mission
+        this.students = 0
+        this.lives = 0
+    }
+    get getStudents(){
+        return this.students
+    }
+    get getLives(){
+        return this.lives
+    }
+    set setStudents(students){
+        this.students = students
+    }
+    set setLives(lives){
+        this.lives = lives
+    }
+}
+console.log("Ejemplo 7: Setters para modificar los atributos del objeto")
+const missionCommanderNode = new MissionCommander("Carlo", "NodeJS")
+console.log(missionCommanderNode.getStudents) // 0 por default
+console.log(missionCommanderNode.getLives) // 0 por default
+// actualizamos los atributos por medio de los setters
+console.log("----------------------------------------------")
+missionCommanderNode.setStudents = 100
+missionCommanderNode.setLives = 3
+console.log(missionCommanderNode.getStudents)
+console.log(missionCommanderNode.getLives) 
