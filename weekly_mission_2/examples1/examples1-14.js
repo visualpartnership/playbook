20a5b439286ec3005dc2d4c4d416c6164db3c53b
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-19 02:35:52 -0500

    Agregando ejemplo 14 de examples1.js

---
+++ weekly_mission_2/examples1/examples1-14.js
@@ -0,0 +1,15 @@
+console.log("Operadores")
+
+console.log("--------------------------------------------------------")
+
+console.log("Ejemplo 4: Uso de some")
+console.log("este método validará todos los elementos de la lista")
+console.log("y si alguno cumple con la validación indicada, regresará true, de lo contrario será false.")
+
+console.log("--------------------------------------------------------")
+
+//Lista de elementos
+const bools = [true, true, false, true]
+// Uso de Some para ver si al menos uno de los elementos es false
+const areSomeTrue = bools.some((b) => b === false)
+console.log("Ejemplo 14: Alguno de los elementos en el array es false: " + areSomeTrue) //true
