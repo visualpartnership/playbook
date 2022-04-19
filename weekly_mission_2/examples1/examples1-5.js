eb29c6fd2845610d00d56effb2013b69d7b61427
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-18 23:42:44 -0500

    Agregando ejemplo 5 de examples1.js

---
+++ weekly_mission_2/examples1/examples1-5.js
@@ -0,0 +1,12 @@
+console.log("Operadores")
+
+console.log("--------------------------------------------------------")
+
+console.log("Ejemplo 5: Uso de map para convertir todos los nombres de una lista a minúsculas")
+
+console.log("--------------------------------------------------------")
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const namesToUpperCase = names.map((name) => name.toLowerCase())
+console.log("Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas")
+console.log(namesToUpperCase)
