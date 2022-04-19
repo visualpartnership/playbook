71a309ed8028435d158052e376e78de9d6fd71e4
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-19 16:29:23 -0500

    Agregando ejemplo 2 de examples2.js

---
+++ weekly_mission_2/examples2/examples2-2.js
@@ -0,0 +1,14 @@
+console.log("Clases y objetos")
+
+console.log("----------------------------------------------")
+
+console.log("Ejemplo 2: Crear un objeto a partir de una clase")
+
+console.log("----------------------------------------------")
+
+class Pet{
+
+}
+const myPet1 = new Pet() //Puedo crear muchos objetos de la clase pet
+console.log("Ejemplo 2: Crear un objeto a partir de una clase")
+console.log(myPet1) // Un objeto de la clase pet
