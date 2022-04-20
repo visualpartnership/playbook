f7c1f65bbda42e30b1163944a73cd5f3c0538be2
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-20 18:18:16 -0500

    Agregando los objetos de JS GitHub de exercises1

---
+++ weekly_mission_2/exercises/exercises1/GitHub/repo.js
@@ -0,0 +1,25 @@
+//Modelado de información a Objeto JS
+
+//Modelado de GitHub => repo
+
+const repo = {
+    name: "LaunchX",
+    author: "GloriaCoralCerecedo",
+    languaje: "JavaScript",
+    numberOfCommits: 100,
+    stars: 50,
+    forks: 30,
+    issues_open: 10,
+    issues_close: 10,
+    getTotalIssues: function() {
+        return this.issues_open + this.issues_open
+    },
+    getGeneralInfo: function(){
+        return `This repository ${this.name} was created by ${this.author}`
+    }
+}
+console.log(repo)
+console.log("------------------------------------------")
+console.log("Nombre del repo: " + repo.name)
+console.log("Issues totales: " + repo.getTotalIssues())
+console.log(repo.getGeneralInfo())
