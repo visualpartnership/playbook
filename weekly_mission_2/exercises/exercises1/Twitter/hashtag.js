184d09fed67586c362fdc89a2b74809d2559f691
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-21 01:09:44 -0500

    Agregando objeto de JS (hashtag) Twitter de exercises1

---
+++ weekly_mission_2/exercises/exercises1/Twitter/hashtag.js
@@ -0,0 +1,20 @@
+//Modelado de información a Objeto JS
+
+//Modelado de Twitter => hashtag
+
+const hashtag = {
+    word: "Music",
+    phrase: "MusicIsLove",
+    numTweetsLastHour: 2000,
+    getGeneralInfo: function(){
+        return `This # ${this.word} have ${this.numTweetsLastHour} of tweets in the last our`
+    },
+    getPhrase: function(){
+        return this.phrase
+    }
+}
+console.log(hashtag)
+console.log("--------------------------------------------")
+console.log("The top phrase in this moment is " + hashtag.getPhrase())
+console.log("--------------------------------------------")
+console.log(hashtag.getGeneralInfo())
