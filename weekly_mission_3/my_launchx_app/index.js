b78a245db62ecf7cdaa5732664651144872c1b70
RogelioBadachi <royer2302@gmail.com>
2022-04-22 00:47:09 -0700

    Paso 7: Importar clase e instanciar tres objetos imprimiendo atributo 'name'

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const roger = new MissionCommander("Roger")
+const herbert = new MissionCommander("Herbert")
+console.log(carlo.name)
+console.log(roger.name);
+console.log(herbert.name);
