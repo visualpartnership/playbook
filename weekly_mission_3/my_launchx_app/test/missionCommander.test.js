724d832b8742685fb75a90cf79bb2c0a63a743a7
RogelioBadachi <royer2302@gmail.com>
2022-04-23 23:50:35 -0700

    Paso 15: Modifique las descripciones de la prueba

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -1,8 +1,11 @@
 const MissionCommander = require('./../app/missionCommander');
+describe("Unit Tests for Mission Commander Class", () => {
+    test('1) Create a mission commander objet', () => {
 
+    },
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
         const result = 1 + 2 
         expect(result).toBe(10);
     });
-})
+}))})
