53cc56a27f9daf38a6cf103fd726c2aaee1e966d
RogelioBadachi <royer2302@gmail.com>
2022-04-23 23:45:58 -0700

    Paso 14: importar clase en linea 1 de 'test/missionCommander.test.js'

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -1,3 +1,5 @@
+const MissionCommander = require('./../app/missionCommander');
+
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
         const result = 1 + 2 
