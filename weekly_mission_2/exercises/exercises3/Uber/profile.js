eb13ca55ffb1cf83910cc7c986d016a5873adc89
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2022-04-23 22:51:32 -0500

    Agregando class Profile

---
+++ weekly_mission_2/exercises/exercises3/Uber/profile.js
@@ -0,0 +1,19 @@
+// Convirtiendo objeto profile a definición de clase Profile
+
+export default class Profile{
+    constructor(name, lastname, phone, email, location, numTravels){
+    this.name = name
+    this.lastname = lastname
+    this.phone = phone
+    this.email = email
+    this.location = location
+    this.numTravels = numTravels
+    }
+
+    getNameAndLocation(){
+        return `The user ${this.name} is of ${this.location}`
+    }
+    getGeneralInfo(){
+        return `The data of profile of ${this.name} ${this.lastName} are: email ${this.email}, phone ${this.phone} and her number of travels is ${this.myNumtravels}`
+    }
+}
