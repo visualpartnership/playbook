c36c431e3d7acb53191fbb99efc607496642cbb5
RogelioBadachi <royer2302@gmail.com>
2022-04-26 13:25:28 -0700

    Paso |||1-5 Modifique prueba de unidad agregando descripcion mas explicita del caso que quiero probar

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,14 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18);
+describe("Unit Tests for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+        // Aquí escribimos el código que queremos usar tal cual
+        // Quiero poder instanciar un objeto Spiderman con esta información
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+        
+        // Validamos que este código funcione de la forma esperada    
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe (31)
+        expect (andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe (31)
+        expect (andrewGarfield.studio).toBe("Sony")
     });
 })
