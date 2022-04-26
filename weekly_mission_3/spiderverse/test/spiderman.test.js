c5777b17c78334f08f06d8b6f5d836d35d48ec69
RogelioBadachi <royer2302@gmail.com>
2022-04-26 16:17:22 -0700

    Arregle la prueba para que la pase

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -11,7 +11,7 @@ describe("Unit Tests for Spiderman class", () => {
         expect(andrewGarfield.name).toBe("Spiderman Sony")
         expect(andrewGarfield.age).toBe(31)
         expect (andrewGarfield.actor).toBe("Andrew Garfield")
-        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.movies).toBe(2)
         expect (andrewGarfield.studio).toBe("Sony")
     });
 })
