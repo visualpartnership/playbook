c07555523c8f4429cfbf9168d718df5f54011d68
Yael Villarreal <iaeloko@gmail.com>
2022-04-25 22:48:25 -0500

    test

---
+++ weekly_mission_3/lives/marvel/app/superhero.js
@@ -0,0 +1,11 @@
+class Superhero {
+
+    constructor (name,heroName,actor){
+        this.name = name
+        this.heroName = heroName
+        this.actor = actor
+    }
+
+}
+
+module.exports = Superhero
