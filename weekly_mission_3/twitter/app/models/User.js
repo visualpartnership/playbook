a96cdb9fab0c50c687cf8bd574d5402d39fafd99
RogelioBadachi <royer2302@gmail.com>
2022-04-27 16:44:07 -0700

    Agregue en el archivo 'user.js' la clase 'User' y se verifico que la prueba paso

---
+++ weekly_mission_3/twitter/app/models/User.js
@@ -0,0 +1,12 @@
+class User {
+    constructor (id, username, name, bio, dateCreated, lastUpdated) {
+    this.id = id;
+    this.username = username;
+    this.name = name;
+    this.bio = bio;
+    this.dateCreated = dateCreated;
+    this.lastUpdated = lastUpdated;
+    }
+}
+
+module.exports = User
--- weekly_mission_3/twitter/test/user.test.js
+++ weekly_mission_3/twitter/test/user.test.js
@@ -1,3 +1,5 @@
+const User = require('./../app/models/user')
+
 describe("Unit Tests for User class", () => {
     test('1) Create an User object', () => {
         // Aquí invocas el código que vas a usar en tu app
@@ -6,7 +8,6 @@ describe("Unit Tests for User class", () => {
         // Aquí validas los resultados de ese código
         /* Esta es una comparación que va a igualar el valor de la izquierda 
         con el valor de la derecha (valor esperado) */
-        
         expect(user.id).toBe(1)
         expect(user.username).toBe("carlogilmar")
         expect (user.name).toBe("Carlo")
