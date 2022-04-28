e6f1674e707c40b2621065636217e48a4d69ebbe
RogelioBadachi <royer2302@gmail.com>
2022-04-28 16:25:24 -0700

    Requerimiento 3: Agregue prueba 'Add getters' y verifique que falla

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -13,5 +13,12 @@ describe("Unit Tests for User class", () => {
         expect (user.name).toBe("Carlo")
         expect(user.dateCreated).not.toBeUndefined()
         expect (user.lastUpdated).not.toBeUndefined()
+    }),
+    test('Add getters', () => {
+        const user = new User( 1, "carlogilmar", "Carlo", "Bio")
+        expect (user.getUsername).toBe("carlogilmar")
+        expect (user.getBio).toBe("Bio")
+        expect (user.getDateCreated).not.toBeUndefined()
+        expect (user.getLastUpdated).not.toBeUndefined()
     })
 })
