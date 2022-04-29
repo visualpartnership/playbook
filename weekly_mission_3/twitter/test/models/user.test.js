f6395ee8a62a2e482b0ccaa8dd9752c03f91e53f
RogelioBadachi <royer2302@gmail.com>
2022-04-28 18:44:23 -0700

    Requerimiento 4: Agregue caso de prueba 'setters' y verifique que falle

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -20,5 +20,13 @@ describe("Unit Tests for User class", () => {
         expect (user.getBio).toBe("Bio")
         expect (user.getDateCreated).not.toBeUndefined()
         expect (user.getLastUpdated).not.toBeUndefined()
+    }),
+        test('Add setters', () => {
+        const user = new User( 1, "carlogilmar", "Carlo", "Bio")
+        user.setUsername = "Gilmar"
+        expect(user.username).toBe("Gilmar")
+
+        user.setBio = "New bio"
+        expect(user.bio).toBe("New bio")
     })
 })
