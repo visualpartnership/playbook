625804b90ccba418f5c3a2c418f7841c5bd51a5a
RogelioBadachi <royer2302@gmail.com>
2022-04-28 18:54:14 -0700

    Requerimiento 4: Agregue codigo para hacer pasar la prueba

--- weekly_mission_3/twitter/app/models/User.js
+++ weekly_mission_3/twitter/app/models/User.js
@@ -20,7 +20,12 @@ class User {
     get getLastUpdated () {
         return this.lastUpdated
         }
+    set setUsername(newUsername){
+        this.username = newUsername
+    }
+    set setBio(newBio){
+        this.bio = newBio
+    }
 }
-
 
 module.exports = User
