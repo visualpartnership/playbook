c51bd220b558b94dd2b623b2bd77042da34cc392
RogelioBadachi <royer2302@gmail.com>
2022-05-02 00:28:53 -0700

    Requerimiento 4: añadi test 4 y revise que falle

--- weekly_mission_3/services/test/services/UserService.test.js
+++ weekly_mission_3/services/test/services/UserService.test.js
@@ -22,6 +22,15 @@ describe("Tests for UserService", () => {
         expect(user.username).toBe("carlog")
         
 
+    }),
+    test("4. Given a list of users give me the list of usernames", () => {
+        const user1 = UserService.create(1, "carlogilmar1", "Carlo")
+        const user2 = UserService.create (1, "carlogilmar2", "Carlo")
+        const user3 = UserService.create (1, "carlogilmar3", "Carlo")
+        const usernames= UserService.getAllUsernames ([user1, user2, user3])
+        expect(usernames).toContain("carlogilmar1")
+        expect(usernames).toContain("carlogilmar2")
+        expect (usernames).toContain("carlogilmar3")
     })
 
 })
