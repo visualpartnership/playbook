303fc1fe6b29d7c1124e9c727f80c6e1f9d2210d
RogelioBadachi <royer2302@gmail.com>
2022-05-01 22:15:42 -0700

    Agregue test 3 y verifique que falla

--- weekly_mission_3/services/test/services/UserService.test.js
+++ weekly_mission_3/services/test/services/UserService.test.js
@@ -15,8 +15,13 @@ describe("Tests for UserService", () => {
         expect(userInfoInList[1]).toBe("carlogilmar")
         expect(userInfoInList[2]).toBe("Carlo")
         expect(userInfoInList[3]).toBe("Sin bio")
+    }),
+    test("3. Update username", () => {
+        const user = UserService.create (1, "carlogilmar", "Carlo")
+        UserService.updateUserUsername(user, "carlog")
+        expect(user.username).toBe("carlog")
+        
+
     })
 
-}
-
-)
+})
