a6360aa597454de65ff397737ae9744792ae5413
RogelioBadachi <royer2302@gmail.com>
2022-05-08 00:45:43 -0700

    Agregue prueba 2 e intento de solucion para que pase la prueba

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -4,7 +4,9 @@ class UserView {
         if(payload === null){
             console.log("Error es null")
             return{error: "El payload no existe"}
-        }
+        } else if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number') {
+            return UserService.create(payload.id, payload.username, payload.name)
+            }
     }
 }
 module.exports = UserView
--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -6,5 +6,10 @@ describe("Tests for UserView",()=>{
         const result=UserView.createUser(payload)
         // https:// jest js.io/docs/using-matchers#strings
         expect(result.error).toMatch(/payload no existe/)
+    }),
+    test("Return an error object when try to create a new user with a payload with invalid properties", ()=>{
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
     })
 })
