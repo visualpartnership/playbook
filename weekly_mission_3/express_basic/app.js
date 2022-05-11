37cd84b00cce20eefb696d71ba4197b0b88c48b1
RogelioBadachi <royer2302@gmail.com>
2022-05-11 14:24:52 -0700

    Agregue una nueva ruta

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -4,11 +4,17 @@ const express=require('express')
 const app=express()
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
-// path inicial, responderáala url localhost:3000
+// path inicial, responderá a la url localhost:3000
 app.get('/', (req, res) =>{
     res.send( 'Hello World! ')
 })
 // Con esto inicializamos esta app
 app.listen(port, () =>{
     console.log( `Example app listening on port ${port}`)
+})
+
+// Respondiendo texto
+// localhost:3000/launchx
+app.get('/launchx', (req,res) => {
+    res.send('Bienvenidos a launchx')
 })
