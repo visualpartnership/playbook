0d8e4c17f6b3770226eb04e5c1b35f9f568e03d9
RogelioBadachi <royer2302@gmail.com>
2022-05-11 23:18:54 -0700

    POST Cree un endpoint que se encarga de crear un explorer

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -17,6 +17,19 @@ app.get('/v1/explorers', (req,res) => {
     res.status(200).json(explorers)
 })
 
+app.get('/v1/explorers/:id', (req,res) => {
+    console.log(`Api Explorers GET request ${new Date()}`);
+    console.log(`Getting explorer with id ${req.params.id}`);
+    const explorer = {id: 1, name: "Carlo"}
+    res.status(200).json(explorer)
+})
+
+app.post('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers POST request ${new Date()}`);
+    const requestBody = req.body // Parámetros de un cliente
+    res.status(201).json({message: "Created"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
