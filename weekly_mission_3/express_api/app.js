23bb795519417f42c3133f5f0776ea5469dab4c3
RogelioBadachi <royer2302@gmail.com>
2022-05-11 23:31:08 -0700

    Cree un endpoint que se encargue de actualizar un explorer

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -30,6 +30,13 @@ app.post('/v1/explorers', (req,res) => {
     res.status(201).json({message: "Created"})
 })
 
+app.put('/v1/explorers/:id', (req,res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`);
+    console.log(`Update explorer with id ${req.params.id}`);
+    const requestBody = req.body // Parámetros de un cliente
+    res.status(200).json({message: "Updated!"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
