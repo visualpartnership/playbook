c68f18e33c38a63765fa52179668b50216db1047
RogelioBadachi <royer2302@gmail.com>
2022-05-11 19:53:59 -0700

    Cree un endpoint que regresa lista de explorers con metodo GET

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -6,6 +6,17 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
+// HTTP METHODS
+app.get('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers GET ALL requests ${new Date()}`);
+    const explorer1 = {id: 1, name: "Carlo1"}
+    const explorer2 = {id: 2, name: "Carlo2"}
+    const explorer3 = {id: 3, name: "Carlo3"}
+    const explorer4 = {id: 4, name: "Carlo4"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
