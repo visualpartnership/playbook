3654f0a258786b302ed0a418f1ad5131fc71afbe
RogelioBadachi <royer2302@gmail.com>
2022-05-11 19:25:17 -0700

    Agregue archivo 'app.js' y app de express

---
+++ weekly_mission_3/express_api/app.js
@@ -0,0 +1,12 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
